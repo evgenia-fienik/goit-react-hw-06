c90747bcaeb484fa63faa21eb9273e2ed5241a99
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-09-16 23:13:09 +0300

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from 'react'
-import ContactList from './ContactList/ContactList';
-import ContactForm from './ContactForm/ContactForm';
-import SearchBox from './SearchBox/SearchBox';
+import ContactList from './components/ContactList/ContactList';
+import ContactForm from './components/ContactForm/ContactForm';
+import SearchBox from './components/SearchBox/SearchBox';
 import { useSelector, useDispatch } from 'react-redux';
 import { addContact, deleteContact } from './redux/contactsSlice';
 import { changeFilter, selectNameFilter } from './redux/filtersSlice';
